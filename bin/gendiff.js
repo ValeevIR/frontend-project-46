9a0beb38df3b3cff2d8a5182f8c2ec70ab34c97f
Ilgiz Valeev <skif-vi@yandex.ru>
2025-07-10 15:35:29 +0500

    step 2 file gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Test');
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .description('Compares two configuration files a difference.')
+
+program.parse();
